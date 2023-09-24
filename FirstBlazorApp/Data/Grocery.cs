5bb601903620ef836af9eb36bb7bef39da3c3c0f
DEshanm123 <deshanm123@gmail.com>
2023-09-24 11:17:47 +0100

    Add project files.

---
+++ FirstBlazorApp/Data/Grocery.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstBlazorApp.Data
+{
+    public class Grocery
+    {
+        [Required]
+        [StringLength(15,"Grocery Name should be less than 15 characters")];
+        public string Name { get; set; }
+        public float Price { get; set; }
+    }
+}
