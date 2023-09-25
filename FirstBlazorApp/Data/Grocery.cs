39baa17c69995f212a8f73fb684545cb2573596e
DEshanm123 <deshanm123@gmail.com>
2023-09-25 20:01:36 +0100

    implementing css

--- FirstBlazorApp/Data/Grocery.cs
+++ FirstBlazorApp/Data/Grocery.cs
@@ -5,8 +5,11 @@ namespace FirstBlazorApp.Data
     public class Grocery
     {
         [Required]
-        [StringLength(15,"Grocery Name should be less than 15 characters")];
+        [StringLength(15, ErrorMessage ="Grocery Name should be less than 15 characters")]
         public string Name { get; set; }
+        
+        [Required]
+        [Range(1,1000, ErrorMessage = "The Price of the item should be between 1 and 10000")]
         public float Price { get; set; }
     }
 }
